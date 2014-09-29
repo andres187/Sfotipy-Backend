2246cdf9610e9294492c92f4256eb5dce488370c
andres187 <andresfebo187@gmail.com>
2014-09-29 17:11:23 -0500

    Modelos subidos de la base de datos aun sin vistas

---
+++ artists/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Artist(models.Model):
+	first_name = models.CharField(max_length=255)
+	second_name = models.CharField(max_length=255, blank=True)
+	biography = models.TextField(blank=True)
