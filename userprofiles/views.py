0df7d37536ffe82036f44cbbba176e8e7758250b
andres187 <andresfebo187@gmail.com>
2014-10-05 16:57:03 -0500

    Hicimos el formulario de signin y pudimos entrar desde el signin al admin de django, y creamos el backend para loguear

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login
 
-from .forms import UserCreationEmailForm
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
 
 def signup(request):
 	form = UserCreationEmailForm(request.POST or None)
@@ -9,4 +10,14 @@ def signup(request):
 		form.save()
 
 	return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+	form = EmailAuthenticationForm(request.POST or None)
+
+	if form.is_valid():
+		login(request, form.get_user())
+
+	return render(request, 'signin.html', {'form': form})
+
+
 
