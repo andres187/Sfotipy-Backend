fe242c4a114b6ffb253c06a2a065aab203518b0b
andres187 <andresfebo187@gmail.com>
2014-10-14 10:55:03 -0500

    Creado un middlewares y se trabajo un nuevo metodo de generar vista que es el DetailView

---
+++ sfotipy/middleware.py
@@ -0,0 +1,12 @@
+from random import choice
+from django.shortcuts import redirect
+paises = ['Colombia', 'Peru', 'Mexico']
+
+def de_donde_vengo(request):
+	return choice(paises)
+
+class PaisMiddleware():
+	def process_request(self, request):
+		pais = de_donde_vengo(request)
+		if pais == 'Colombia':
+			return redirect('http://libidohouse.net')
--- tracks/views.py
+++ tracks/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404
 
 from .models import Track
 
+
 def track_view(request, title):
 	track = get_object_or_404(Track, title=title)
 
